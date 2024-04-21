8ef67d3d56948a47dda9ecb9e4aa6ab4b94bc9ca
Brayan Quevedo Ramos <brayanquevedo2134@gmail.com>
2024-04-21 00:12:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -69,4 +69,6 @@ def ask():
 
 
 if __name__ == '__main__':
-    app.run(debug=False)  # Debug mode should only be used for development
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    #app.run(debug=True)  # Debug mode should only be used for development
